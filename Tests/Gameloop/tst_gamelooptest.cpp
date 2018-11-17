925f8a4f936608412e8fb019bc9faded06d69f1c
Hamiller78 <Kneesch@gmx.de>
2018-11-17 18:18:55 +0100

    Change and extend Gameloop unit test
    The test will use two player AIs and AI looking 3 turns into the future of the game.

--- Tests/Gameloop/tst_gamelooptest.cpp
+++ Tests/Gameloop/tst_gamelooptest.cpp
@@ -22,6 +22,18 @@
 
 #include <QtTest>
 
+// test game logic is in mock classes:
+// - only Pointcounter(0) of GameStates is used
+// - Gamemovemock alters the point value
+// - Rulebookmock provides only two sets of moves:
+//   - Even Gamestates: +4/-2/+1
+//   - Odd Gamestates: -6/+2
+// - Playeragentmock uses Pointcounter(0) as rating value
+// - Win condition: >20 points: player 1, <-20 points: player 2
+// -> Even states lead to player 1 winning, odd states to player 2 winning
+// - Player 2 will have to use the +1 move once to get the game on the odd states
+// - Player 2 should only see this, when the move depth its looking into is at least 3
+
 using namespace game;
 
 class GameloopTest : public QObject
@@ -29,10 +41,7 @@ class GameloopTest : public QObject
     Q_OBJECT
 
 private:
-    std::shared_ptr<Gameloop> testLoop_;
     std::shared_ptr<Rulebook> testRulebook_;
-    std::shared_ptr<PlayerAgent> testPlayExpert_;
-    std::shared_ptr<GameState> startState_;
 
 public:
     GameloopTest();
@@ -41,7 +50,8 @@ public:
 private slots:
     void initTestCase();
     void cleanupTestCase();
-    void test_RunGame();
+    void test_RunGameAiDepth1();
+    void test_RunGameAiDepth3();
 
 };
 
@@ -58,24 +68,33 @@ GameloopTest::~GameloopTest()
 void GameloopTest::initTestCase()
 {
     testRulebook_ = std::shared_ptr<Rulebook>(new RulebookMock());
-    testPlayExpert_ = std::shared_ptr<PlayerAgent>(new PlayerAgentMock());
-    testLoop_ = std::shared_ptr<Gameloop>(new Gameloop(*testRulebook_, *testPlayExpert_));
-    startState_ = std::shared_ptr<GameState>(new GameState());
 }
 
 void GameloopTest::cleanupTestCase()
 {
-    testLoop_.reset();
-    startState_.reset();
-    testPlayExpert_.reset();
     testRulebook_.reset();
 }
 
-void GameloopTest::test_RunGame()
+void GameloopTest::test_RunGameAiDepth1()
 {
-    // TODO: Mock objects should check that the expected moves are executed
-    int Winner = testLoop_->RunGame(*startState_);
+    PlayerAgentMock testPlayer1(1);
+    PlayerAgentMock testPlayer2(1);
+    GameState testStartState;
+    Gameloop testLoop(*testRulebook_, testPlayer1, testPlayer2);
+
+    int Winner = testLoop_->RunGame(testStartState);
     QVERIFY2(Winner == 1, "The winner of the game was not player 1!");
+}
+
+void GameloopTest::test_RunGameAiDepth3()
+{
+    PlayerAgentMock testPlayer1(3);
+    PlayerAgentMock testPlayer2(3);
+    GameState testStartState;
+    Gameloop testLoop(*testRulebook_, testPlayer1, testPlayer2);
+
+    int Winner = testLoop_->RunGame(testStartState);
+    QVERIFY2(Winner == 2, "The winner of the game was not player 2!");
 }
 
 QTEST_APPLESS_MAIN(GameloopTest)
