b2ceb7582d064475783df5a059df05e09972b615
Hamiller78 <Kneesch@gmx.de>
2020-01-05 22:58:00 +0100

    Add Pile tests for GoogleTests
    Made a GoogleTest version of all existing unit tests for QTest.
    May add more.
    Going to replace Gamepiece class by a GoogleMock mock.

--- GoogleTests/tst_pile.cpp
+++ GoogleTests/tst_pile.cpp
@@ -20,7 +20,7 @@
 #include "../Prototype/gamepiece.h"
 #include "../Prototype/pile.h"
 
-TEST(Pile,AddGamepieces)
+TEST(Pile, AddGamepieces)
 {
     game::Pile testPile1;
     std::shared_ptr<game::Gamepiece> testPiecePtr1(new game::Gamepiece);
@@ -34,4 +34,59 @@ TEST(Pile,AddGamepieces)
     testPile1.AddOnTop(testPiecePtr3);
     ASSERT_EQ(testPiecePtr3, testPile1.GetTopPiece());
     ASSERT_EQ(3, testPile1.GetPilesize());
+}
+
+TEST(Pile, AddPile)
+{
+    game::Pile testPile1;
+    game::Pile testPile2;
+    std::shared_ptr<game::Gamepiece> testPiecePtr1(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr2(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr3(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr4(new game::Gamepiece);
+
+    testPile1.AddOnTop(testPiecePtr1);
+    testPile1.AddOnTop(testPiecePtr2);
+    testPile2.AddOnTop(testPiecePtr3);
+    testPile2.AddOnTop(testPiecePtr4);
+
+    testPile2.AddPile(testPile1);
+    ASSERT_EQ(4, testPile2.GetPilesize());
+    ASSERT_EQ(testPiecePtr2, testPile2.GetTopPiece());
+}
+
+TEST(Pile, PickupTopPiece)
+{
+    game::Pile testPile1;
+    std::shared_ptr<game::Gamepiece> testPiecePtr1(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr2(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr3(new game::Gamepiece);
+
+    testPile1.AddOnTop(testPiecePtr1);
+    testPile1.AddOnTop(testPiecePtr2);
+    testPile1.AddOnTop(testPiecePtr3);
+    std::shared_ptr<game::Gamepiece> checkPiece = testPile1.PickupTopPiece();
+    ASSERT_EQ(2, testPile1.GetPilesize());
+    ASSERT_EQ(testPiecePtr2, testPile1.GetTopPiece());
+    ASSERT_EQ(testPiecePtr3, checkPiece);
+}
+
+TEST(Pile, AddToTopAndBottomAndGetVector)
+{
+    game::Pile testPile1;
+    std::shared_ptr<game::Gamepiece> testPiecePtr1(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr2(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr3(new game::Gamepiece);
+    std::shared_ptr<game::Gamepiece> testPiecePtr4(new game::Gamepiece);
+
+    testPile1.AddOnTop(testPiecePtr1);
+    testPile1.AddToBottom(testPiecePtr2);
+    testPile1.AddOnTop(testPiecePtr3);
+    testPile1.AddToBottom(testPiecePtr4);
+    std::vector<std::shared_ptr<game::Gamepiece>> pieceList = testPile1.GetPieceVector();
+
+    ASSERT_EQ(testPiecePtr4, pieceList[0]);
+    ASSERT_EQ(testPiecePtr2, pieceList[1]);
+    ASSERT_EQ(testPiecePtr1, pieceList[2]);
+    ASSERT_EQ(testPiecePtr3, pieceList[3]);
 }
